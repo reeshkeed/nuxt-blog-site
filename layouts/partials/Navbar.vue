f997dbb8ba4da23f83219cfc5ef7a01d0560f9ec
reeshkeed <girishdaloso@hotmail.com>
2019-10-27 05:36:45 +0800

    Update navbar logo and background color

--- layouts/partials/Navbar.vue
+++ layouts/partials/Navbar.vue
@@ -1,31 +1,17 @@
 <template lang="html">
   <div>
-    <nav class="flex items-center justify-between flex-wrap bg-black p-5 fixed top-0 left-0 right-0">
+    <nav class="flex items-center justify-between flex-wrap bg-indigo-800 p-5 fixed top-0 left-0 right-0">
       <div class="flex items-center flex-shrink-0 text-white mr-6">
-        <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-        <span class="font-semibold text-xl tracking-tight">Blog</span>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" viewBox="0 0 302.7 302.7">
+          <path fill="#FFF" d="M168.7 6.6c18.3 0 32.9 2.8 43.8 8.3 13.1 6.7 23.1 15.8 30.2 27.5 7.1 11.7 10.6 24.7 10.6 39.2 0 13.6-3.6 26.2-10.8 37.7-7.2 11.5-17.1 20.6-29.6 27.3 15 5.3 27.6 14.5 37.9 27.5 10.3 13.1 15.4 27.8 15.4 44.2 0 14.5-3.7 27.7-11 39.8-7.4 12.1-17.2 21.6-29.4 28.6-6.1 3.3-12.9 5.8-20.4 7.5-7.5 1.7-17.5 2.5-30 2.5H46.9V6.6h121.8z"/>
+        </svg>
+        <span class="font-light text-xl mx-2 tracking-tight">nuxt-blog-site</span>
       </div>
-      <div class="block lg:hidden">
-        <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-        </button>
-      </div>
-      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div class="text-sm lg:flex-grow">
-          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-            Docs
-          </a>
-          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-            Examples
-          </a>
-          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-            Blog
-          </a>
-        </div>
+      <div class="w-full lg:flex lg:items-right lg:w-auto">
         <div>
-          <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Login</a>
-          <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Register</a>
-          <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Logout</a>
+          <a href="#" class="inline-block text-sm px-5 py-3 leading-none border bg-indigo-400 rounded text-white border-indigo-400 hover:border-transparent hover:text-indigo-400 hover:bg-white mt-4 lg:mt-0">Login</a>
+          <a href="#" class="inline-block text-sm px-5 py-3 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-400 hover:bg-white mt-4 lg:mt-0">Register</a>
+          <a href="#" class="inline-block text-sm px-5 py-3 leading-none rounded text-indigo-600 hover:text-white mt-4 lg:mt-0">Logout</a>
         </div>
       </div>
     </nav>
