c42208aedc7434a241cdd37a8edc61be405cdb6c
reeshkeed <girishdaloso@hotmail.com>
2019-10-29 03:55:18 +0800

    Added display error on submit form

---
+++ components/Notification.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div class="">
+    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 mb-5 rounded relative" role="alert">
+      <strong class="font-bold">{{ message.error }}</strong>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Notification',
+  props: ['message']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
