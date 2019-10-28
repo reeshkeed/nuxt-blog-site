ee24865bdd4518c75f6eb5f8dfaf7cc508bed60a
reeshkeed <girishdaloso@hotmail.com>
2019-10-28 23:33:33 +0800

    Added displaying articles

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,55 @@
 <template>
   <section class="container mx-auto">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        nuxt-blog-site
-      </h1>
-      <h2 class="subtitle">
-        Blog site
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+    <div class=" flex flex-col w-full px-16">
+      <div
+       class="border border-gray-400 w-full rounded text-left px-4 py-2 m-2"
+        v-for="(list, i) in article"
+        :key="i"
+      >
+        <div>
+          <p>{{ list.title }}</p>
+          <p>{{ list.description }}</p>
+          <p>{{ list.created_at | formatDate }}</p>
+        </div>
+
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import AppLogo from '~/components/AppLogo.vue';
+import Vue from 'vue'
+import dayjs from 'dayjs';
+import relativeTimePlugin from 'dayjs-ext/plugin/relativeTime';
 
 export default {
+  data: () => ({
+    article: [],
+  }),
+
+  async mounted () {
+    const response = await this.$axios.get('articles');
+    this.article = response.data;
+
+    const toISOString = (value) => {
+      const date = new Date(value);
+
+      const year = date.getFullYear();
+      const month = date.getMonth();
+      const day = date.getDate();
+      const hours = date.getHours();
+      const minutes = date.getMinutes();
+      const seconds = date.getSeconds();
+
+      const utcDate = Date.UTC(year, month, day, hours, minutes, seconds);
+
+      return new Date(utcDate).toISOString();
+    };
+    dayjs.extend(relativeTimePlugin);
+    Vue.filter('formatDate', (value) => dayjs(toISOString(value)).fromNow());
+  },
+
   components: {
     AppLogo
   }
